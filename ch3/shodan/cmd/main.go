80757a6d680487197f844e2491e377eb4786266f
ikalafe <imkalafe@gmail.com>
2025-07-07 18:20:36 -0400

    feat(cli): initialize CLI tool for Shodan API integration

---
+++ ch3/shodan/cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalln("Usage: shodan searchterm")
+	}
+	apiKey := os.Getenv("SHODAN_API_KEY")
+	s := shodan.New
+}
