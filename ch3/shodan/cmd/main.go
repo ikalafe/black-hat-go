ecb129dc4e396ab2bdabf90ece4189fc496c134e
ikalafe <imkalafe@gmail.com>
2025-07-07 20:35:14 -0400

    feat: integrate Shodan client and perform host search by term

--- ch3/shodan/cmd/main.go
+++ ch3/shodan/cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/blackhat-go/bhg/ch-3/shodan/shodan"
 )
 
 func main() {
@@ -10,5 +13,19 @@ func main() {
 		log.Fatalln("Usage: shodan searchterm")
 	}
 	apiKey := os.Getenv("SHODAN_API_KEY")
-	s := shodan.New
+	s := shodan.New(apiKey)
+	info, err := s.APIInfo()
+	if err != nil {
+		log.Panicln(err)
+	}
+	fmt.Printf("Query Creadits: %d\nScan Creadits: %d\n\n", info.QueryCredits, info.ScanCredits)
+
+	hostSearch, err := s.HostSearch(os.Args[1])
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	for _, host := range hostSearch.Matches {
+		fmt.Printf("%18s%8d\n", host.IPString, host.Port)
+	}
 }
